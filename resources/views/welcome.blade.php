ed1314fc7fe27e89e364f32ec2b14690db6b9b17
username-fame-health <romipelajarmr01@gmail.com>
2026-07-27 03:05:28 +0700

    p

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -824,6 +824,7 @@
                     <a href="#alur">Alur</a>
                     <a href="#modul">Modul</a>
                     <a href="#integrasi">API</a>
+                    <a class="button button-outline" href="{{ asset('jupe-bb.apk') }}" download>Unduh APK</a>
                     <a class="button button-primary" href="{{ url('/admin') }}">Masuk Admin</a>
                 </div>
 
@@ -843,7 +844,8 @@
 
                         <div class="hero-actions">
                             <a class="button button-primary" href="{{ url('/admin') }}">Buka Dashboard Admin</a>
-                            <a class="button button-secondary" href="#fitur">Lihat Fitur Utama</a>
+                            <a class="button button-secondary" href="{{ asset('jupe-bb.apk') }}" download>Unduh Aplikasi (APK)</a>
+                            <a class="button button-outline" href="#fitur" style="color: white; border-color: rgba(255,255,255,0.3)">Lihat Fitur</a>
                         </div>
 
                         <div class="hero-metrics" aria-label="Ringkasan kemampuan aplikasi">
