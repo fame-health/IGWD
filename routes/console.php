0ca01aa2d48f361a0100fb39e54f3904ee43893d
username-fame-health <romipelajarmr01@gmail.com>
2026-07-29 06:35:22 +0700

    r

--- routes/console.php
+++ routes/console.php
@@ -11,5 +11,5 @@ Artisan::command('inspire', function () {
 Schedule::command('app:check-missing-daily-monitoring')->dailyAt('20:00');
 Schedule::command('app:send-dialysis-schedule-reminders')->everyFiveMinutes();
 Schedule::command('app:generate-automatic-schedules')
-    ->hourly()
+    ->everyMinute()
     ->timezone(config('hd.timezone', config('app.timezone')));
